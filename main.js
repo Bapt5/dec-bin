8803b50c5e629762a47d51d824c76a230e003075
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2020-10-09 16:35:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+#programme en python
+var b=str("")
+var d=int(input("entrez un nombre decimal entier"))
 
+while(d!=0):
+    rd=d%2
+    b=str(rd)+b
+    d=d//2
+print ("votre nombre decimal s'ecrit",b,"en binaire")
