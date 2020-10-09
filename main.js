d1825ccc5c7358244bb299ed3698a36e05bcb14a
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2020-10-09 16:41:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ function decimal()
     h= hexadecimal
     b= binaire
 }
-var b=str("")
-var d=int(input("entrez un nombre decimal entier"))
 
-while(d!=0):
-    rd=d%2
-    b=str(rd)+b
-    d=d//2
-print ("votre nombre decimal s'ecrit",b,"en binaire")
+function hexadecimal()
+{
+    d= decimal
+    h= hexadecimal
+    b= binaire
+}
+
+function binaire()
+{
+    d= decimal
+    h= hexadecimal
+    b= binaire
+}
