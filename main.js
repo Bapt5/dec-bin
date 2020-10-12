00fc312192ef5ea995560e2a5b16bb81eef9876f
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2020-10-12 13:30:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function decimal()
 {
     var dec = document.getElementById("dec").value;
-    var hex = dec.toString(16);
-    var bin = dec.toString(2);
+    var hex = parseInt(dec-0).toString(16);
+    var bin = parseInt(dec-0).toString(2);
     document.getElementById("bin").value ="";
     document.getElementById("hex").value ="";
     document.getElementById("repBin").innerHTML  = "Votre nombre s'écrit " + bin + " en binaire";
@@ -26,8 +26,8 @@ function hexadecimal()
 function binaire()
 {
     var bin = document.getElementById("bin").value;
-    var dec = parseInt(binaire,2);
-    var hex = parseInt(binaire,2).toString(16);
+    var dec = parseInt(bin,2);
+    var hex = parseInt(bin,2).toString(16);
     document.getElementById("dec").value ="";
     document.getElementById("hex").value ="";
     document.getElementById("repDec").innerHTML  = "Votre nombre s'écrit " + dec + " en décimal";
