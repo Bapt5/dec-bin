e6d73ad14ba53ab715c21bca2c5b0b7c0919ab36
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2020-10-15 14:39:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 function decimal()
 {
     var dec = document.getElementById("dec").value;
-    var hex = parseInt(dec-0).toString(16);
-    var bin = parseInt(dec-0).toString(2);
-    document.getElementById("bin").value ="";
-    document.getElementById("hex").value ="";
-    document.getElementById("repBin").innerHTML  = bin + " en binaire";
-    document.getElementById("repHex").innerHTML  = hex + " en héxadécimal";
-    document.getElementById("repDec").innerHTML  = "";
-    var hex = hex.toString();
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2) {
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    if (dec!=""){
+        var hex = parseInt(dec-0).toString(16);
+        var bin = parseInt(dec-0).toString(2);
+        document.getElementById("bin").value ="";
+        document.getElementById("hex").value ="";
+        document.getElementById("repBin").innerHTML  = bin + " en binaire";
+        document.getElementById("repHex").innerHTML  = hex + " en héxadécimal";
+        document.getElementById("repDec").innerHTML  = "";
+        var hex = hex.toString();
+        var str = '';
+        for (var i = 0; i < hex.length; i += 2) {
+            str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+        }
+        document.getElementById("repAscii").innerHTML  = str + " en ASCII";
+    }else {
+        document.getElementById("repBin").innerHTML  = "";
+        document.getElementById("repHex").innerHTML  = "";
+        document.getElementById("repDec").innerHTML  = "";
+        document.getElementById("repAscii").innerHTML  = "";
     }
-    document.getElementById("repAscii").innerHTML  = str + " en ASCII";    
 }
 
 function hexadecimal()
