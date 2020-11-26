3d82f3a5b86ef2cd493b9d8d2fb245bb7af1d38b
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2020-11-26 15:07:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,86 +1,79 @@
-function decimal()
+function decimal(e)
 {
-    var dec = document.getElementById("dec").value;
-    if (dec!=""){
-        var hex = parseInt(dec-0).toString(16);
-        var bin = parseInt(dec-0).toString(2);
-        document.getElementById("bin").value  = bin;
-        document.getElementById("hex").value  = hex;
-        var hex = hex.toString();
-        var str = '';
-        for (var i = 0; i < hex.length; i += 2) {
-            str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    var touche = event.keyCode;
+    if (touche==13){
+        var dec = document.getElementById("dec").value;
+        if (dec!=""){
+            var hex = parseInt(dec-0).toString(16);
+            var bin = parseInt(dec-0).toString(2);
+            document.getElementById("bin").value  = bin;
+            document.getElementById("hex").value  = hex;
+            var hex = hex.toString();
+            var str = '';
+            for (var i = 0; i < hex.length; i += 2) {
+                str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+            }
+            document.getElementById("ascii").value  = str;
         }
-        document.getElementById("ascii").value  = str;
     }
 }
 
-function hexadecimal()
+function hexadecimal(e)
 {
-    var hex = document.getElementById("hex").value;
-    if (hex!=""){
-    var dec = parseInt(hex,16);
-    var bin = parseInt(hex,16).toString(2);
-    document.getElementById("bin").value  = bin;
-    document.getElementById("dec").value  = dec ;
-    var hex = hex.toString();
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2) {
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    }
-    document.getElementById("ascii").value  = str;
+    var touche = event.keyCode;
+    if (touche==13){
+        var hex = document.getElementById("hex").value;
+        if (hex!=""){
+            var dec = parseInt(hex,16);
+            var bin = parseInt(hex,16).toString(2);
+            document.getElementById("bin").value  = bin;
+            document.getElementById("dec").value  = dec ;
+            var hex = hex.toString();
+            var str = '';
+            for (var i = 0; i < hex.length; i += 2) {
+                str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+            }
+            document.getElementById("ascii").value  = str;
+        }
     }
 }
 
 function binaire()
 {
-    var bin = document.getElementById("bin").value;
-    if (bin!=""){
-    var dec = parseInt(bin,2);
-    var hex = parseInt(bin,2).toString(16);
-    document.getElementById("dec").value  = dec;
-    document.getElementById("hex").value  = hex;
-    var hex = hex.toString();
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2) {
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    }
-    document.getElementById("ascii").value  = str;
+    var touche = event.keyCode;
+    if (touche==13){
+        var bin = document.getElementById("bin").value;
+        if (bin!=""){
+            var dec = parseInt(bin,2);
+            var hex = parseInt(bin,2).toString(16);
+            document.getElementById("dec").value  = dec;
+            document.getElementById("hex").value  = hex;
+            var hex = hex.toString();
+            var str = '';
+            for (var i = 0; i < hex.length; i += 2) {
+                str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+            }
+            document.getElementById("ascii").value  = str;
+        }
     }
 }
 
 function ascii()
 {
-    var str=document.getElementById("ascii").value;
-    if (str!=""){
-    var hex ="";
-    for (var i = 0, l = str.length; i < l; i ++) {
-        var code =Number(str.charCodeAt(i)).toString(16);
-        hex+=code;
-    }
-    document.getElementById("hex").value  = hex;
-    var dec = parseInt(hex,16);
-    var bin = parseInt(hex,16).toString(2);
-    document.getElementById("bin").value  = bin;
-    document.getElementById("dec").value  = dec;
+    var touche = event.keyCode;
+    if (touche==13){
+        var str=document.getElementById("ascii").value;
+        if (str!=""){
+            var hex ="";
+            for (var i = 0, l = str.length; i < l; i ++) {
+                var code =Number(str.charCodeAt(i)).toString(16);
+                hex+=code;
+            }
+            document.getElementById("hex").value  = hex;
+            var dec = parseInt(hex,16);
+            var bin = parseInt(hex,16).toString(2);
+            document.getElementById("bin").value  = bin;
+            document.getElementById("dec").value  = dec;
+        }
     }
 }
-
-var x = setInterval(function() {
-    var dec = document.getElementById("dec").value;
-    var hex = document.getElementById("hex").value;
-    var bin = document.getElementById("bin").value;
-    var str=document.getElementById("ascii").value;
-    if (dec!=""){
-        decimal();
-    }
-    if (hex!=""){
-        hexadecimal();
-    }
-    if (bin!=""){
-        binaire();
-    }
-    if (str!=""){
-        ascii();
-    }
-})
